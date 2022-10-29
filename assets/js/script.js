301aab3aed953966ce5a3f9b5d5f248dccef8504
Jamey Wicklund <theBluWiz@icloud.com>
2022-10-29 08:53:15 -0700

    cAdds time math calculations for each hour of calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
 // Create UI Hooks
-var currentDayEl = $('#currentDay')
-var calendarTable = $('<table>');
-console.log(calendarTable)
+var currentDayEl = $('#currentDay');
+var calendarTable = $('#tbody');
 // Create State Variables
-var currentTime = moment();
+var currentTime = moment().startOf('hour');
 var currentDay = currentTime.format('dddd, MMMM Do')
 console.log(currentTime)
 console.log(currentDay)
 // Set Current Day to top
 currentDayEl.text(currentDay)
 // Set Each time block to past, present, and future
-for (let i = 0; i < calendarTable.length; i++) {
-
+var dayString = currentTime.format('YYYY-MM-DD')
+for (let i = 0; i < 9; i++) {
+  var calHour = moment(dayString + " " + calendarTable.children().eq(i).data("time"));
+  if (moment(calHour).isBefore(currentTime)) {
+    //make red
+  } else if (moment(calHour).isSame(currentTime)) {
+    //keep gray
+  } else {
+    //make green
+  }
 }
+
 // Allow users to create events in persistent storage
+
+
+// console.log(calendarTable.children().eq(i).children().eq(1)) select text content
